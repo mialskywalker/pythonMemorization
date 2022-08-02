c0e634cc2282ba35dc6ffa74354f2d4fba7f2ae9
mialskywalker <mialskywalker@gmail.com>
2022-08-02 17:39:42 +0300

    next

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+name = input()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if name == "Johnny":
+    print("Hello, my love!")
+else:
+    print(f"Hello, {name}!")
