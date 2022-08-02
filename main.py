01b402af1c1636faa2b9ac4be1e636c4ef90a2b9
mialskywalker <mialskywalker@gmail.com>
2022-08-02 18:11:51 +0300

    next

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-divisor = int(input())
-boundary = int(input())
+num_of_orders = int(input())
+total = 0
+total_for_order = 0
 
-num = boundary // divisor
-print(num*divisor)
+for _ in range(num_of_orders):
+    price = float(input())
+    days = int(input())
+    capsules = int(input())
+
+    if 0.01 <= price <= 100.00:
+        if 1 <= days <= 31:
+            if 1 <= capsules <= 2000:
+                total_for_order = price * capsules * days
+
+    if total_for_order > 0:
+        print(f"The price for the coffee is: ${total_for_order:.2f}")
+        total += total_for_order
+
+
+print(f"Total: ${total:.2f}")
