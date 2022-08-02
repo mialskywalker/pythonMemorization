e73e724ed0e7d413efb461bbac95b21190b95ba6
mialskywalker <mialskywalker@gmail.com>
2022-08-02 17:46:08 +0300

    next

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-name = input()
+age = int(input())
 
-if name == "Johnny":
-    print("Hello, my love!")
-else:
-    print(f"Hello, {name}!")
+if age <= 14:
+    print("drink toddy")
+elif 14 < age <= 18:
+    print("drink coke")
+elif 18 < age <= 21:
+    print("drink beer")
+elif age > 21:
+    print("drink whisky")
