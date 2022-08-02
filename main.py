37974e3ab50ca6ee71c0a2030673d164803d9555
mialskywalker <mialskywalker@gmail.com>
2022-08-02 17:58:45 +0300

    next

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-n = int(input())
+divisor = int(input())
+boundary = int(input())
 
-for _ in range(n):
-    num = int(input())
-
-    if num == 88:
-        print("Hello")
-    elif num == 86:
-        print("How are you?")
-    elif num > 88:
-        print("Bye.")
-    else:
-        print("GREAT!")
+num = boundary // divisor
+print(num*divisor)
