1631d5009f9a6dd133b073f8bc4245149bf4d812
mialskywalker <mialskywalker@gmail.com>
2022-08-02 18:47:19 +0300

    next

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-num_of_orders = int(input())
-total = 0
-total_for_order = 0
+n = int(input())
 
-for _ in range(num_of_orders):
-    price = float(input())
-    days = int(input())
-    capsules = int(input())
+for _ in range(n):
 
-    if 0.01 <= price <= 100.00:
-        if 1 <= days <= 31:
-            if 1 <= capsules <= 2000:
-                total_for_order = price * capsules * days
+    text = input()
 
-    if total_for_order > 0:
-        print(f"The price for the coffee is: ${total_for_order:.2f}")
-        total += total_for_order
-
-
-print(f"Total: ${total:.2f}")
+    if "," in text or "." in text or "_" in text:
+        print(f"{text} is not pure!")
+    else:
+        print(f"{text} is pure.")
