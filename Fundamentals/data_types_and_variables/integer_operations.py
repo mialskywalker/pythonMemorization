2b8bf013f11600db51d03133dd31e5d39c7d82de
mialskywalker <mialskywalker@gmail.com>
2022-08-03 16:48:45 +0300

    03.08.2022

---
+++ Fundamentals/data_types_and_variables/integer_operations.py
@@ -0,0 +1,10 @@
+first = int(input())
+second = int(input())
+third = int(input())
+fourth = int(input())
+
+add = first + second
+divide = add // third
+multiply = divide * fourth
+
+print(multiply)
