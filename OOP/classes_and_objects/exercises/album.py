7d0893bda7790c32fb0da6c029810c47c6b220e1
mialskywalker <mialskywalker@gmail.com>
2022-12-01 14:11:20 +0100

    01.12.2022

---
+++ OOP/classes_and_objects/exercises/album.py
@@ -0,0 +1,48 @@
+from OOP.classes_and_objects.exercises.song import Song
+
+
+class Album:
+
+    def __init__(self, name: str, *args):
+        self.name = name
+        self.args = args
+        self.published = False
+        self.songs = [el for el in args]
+
+    def add_song(self, song: Song):
+        if self.published:
+            return f"Cannot add songs. Album is published."
+
+        if song.single:
+            return f"Cannot add {song.name}. It's a single"
+
+        if song in self.songs:
+            return f"Song is already in the album."
+
+        self.songs.append(song)
+        return f"Song {song.name} has been added to the album {self.name}."
+
+    def remove_song(self, song_name: str):
+        if self.published:
+            return f"Cannot remove songs. Album is published."
+
+        for song in self.songs:
+            if song.name == song_name:
+                self.songs.remove(song)
+                return f"Removed song {song_name} from album {self.name}."
+
+        return "Song is not in the album."
+
+    def publish(self):
+        if self.published:
+            return f"Album {self.name} is already published."
+
+        self.published = True
+        return f"Album {self.name} has been published."
+
+    def details(self):
+        result = f"Album {self.name}\n"
+        for s in self.songs:
+            result += f"== {s.get_info()}\n"
+        result = result.strip()
+        return result
