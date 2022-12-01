e3e70d7ef827403a60a4f54ed08a6dca284021c0
mialskywalker <mialskywalker@gmail.com>
2022-12-01 11:35:15 +0100

    01.12.2022

---
+++ OOP/classes_and_objects/exercises/time.py
@@ -0,0 +1,39 @@
+class Time:
+    max_hours = 23
+    max_minutes = 59
+    max_seconds = 59
+
+    def __init__(self, hours: int, minutes: int, seconds: int):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def set_time(self, hours, minutes, seconds):
+        if hours <= Time.max_hours and minutes <= Time.max_minutes and seconds <= Time.max_seconds:
+            self.hours = hours
+            self.minutes = minutes
+            self.seconds = seconds
+
+    def get_time(self):
+        return f"{self.hours:02}:{self.minutes:02}:{self.seconds:02}"
+
+    def next_second(self):
+        if self.seconds == Time.max_seconds:
+            self.seconds = 0
+            self.minutes += 1
+            if self.minutes >= Time.max_minutes:
+                self.minutes = 0
+                self.hours += 1
+                if self.hours >= Time.max_hours:
+                    self.hours = 0
+        else:
+            self.seconds += 1
+        return self.get_time()
+
+
+time = Time(9, 30, 59)
+print(time.next_second())
+time2 = Time(10, 59, 59)
+print(time2.next_second())
+time3 = Time(23, 59, 59)
+print(time3.next_second())
