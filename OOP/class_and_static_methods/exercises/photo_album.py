a89144e112eba9ede3486f11a191ff46409df1f7
mialskywalker <mialskywalker@gmail.com>
2022-12-04 10:54:08 +0100

    04.12.2022

--- OOP/class_and_static_methods/exercises/photo_album.py
+++ OOP/class_and_static_methods/exercises/photo_album.py
@@ -36,6 +36,34 @@ class PhotoAlbum:
             result += '\n-----------\n'
         return result
 
+    # def __init__(self, pages):
+    #     self.pages = pages
+    #     self.photos = [[] for page in range(self.pages)]
+    #     self.max_len = 4
+    #
+    # @classmethod
+    # def from_photos_count(cls, photos_count):
+    #     return cls(int(photos_count / 4))
+    #
+    # def add_photo(self, label):
+    #     for i in range(len(self.photos)):
+    #         if len(self.photos[i]) < 4:
+    #             self.photos[i].append(label)
+    #             return f"{label} photo added successfully on page {i + 1} slot {len(self.photos)}"
+    #     return "No more free slots"
+    #
+    # def display(self):
+    #     result = ''
+    #     for row in self.photos:
+    #         result += f"-----------\n"
+    #         if len(row) > 0:
+    #             result += f"{' '.join(['[]' for el in row])}\n"
+    #         else:
+    #             result += '\n'
+    #     result += f"-----------\n"
+    #     return result
+    #
+
 
 album = PhotoAlbum(2)
 
