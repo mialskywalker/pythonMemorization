50bf4643f5eca517876c952a9d6771e9b5759b23
mialskywalker <mialskywalker@gmail.com>
2022-12-07 14:41:19 +0100

    07.12.2022

---
+++ OOP/encapsulation/exercises/football_team_generator/team.py
@@ -0,0 +1,24 @@
+from OOP.encapsulation.exercises.football_team_generator.player import Player
+
+
+class Team:
+
+    def __init__(self, name: str, rating: int):
+        self.__name = name
+        self.__rating = rating
+        self.__players = []
+
+    def add_player(self, player: Player):
+        if player in self.__players:
+            return f"Player {player.name} has already joined"
+        self.__players.append(player)
+        return f"Player {player.name} joined team {self.__name}"
+
+    def remove_player(self, player_name: str):
+        try:
+            player = [el for el in self.__players if player_name == el.name][0]
+            self.__players.remove(player)
+            return player
+        except IndexError:
+            return f"Player {player_name} not found"
+
