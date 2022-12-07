f50fe853a6f1251f460f47184f52a36a0221d21e
mialskywalker <mialskywalker@gmail.com>
2022-12-07 21:20:11 +0100

    07.12.2022

--- OOP/encapsulation/exercises/restaurant/food/salmon.py
+++ OOP/encapsulation/exercises/restaurant/food/salmon.py
@@ -3,5 +3,7 @@ from project.food.main_dish import MainDish
 
 class Salmon(MainDish):
 
+    GRAMS = 22
+
     def __init__(self, name: str, price: float):
-        super().__init__(name, price, 22)
+        super().__init__(name, price, Salmon.GRAMS)
--- OOP/encapsulation/project/food/cake.py
+++
@@ -1,7 +0,0 @@
-from project.food.dessert import Dessert
-
-
-class Cake(Dessert):
-
-    def __init__(self, name: str):
-        super().__init__(name, 5, 250, 1000)
