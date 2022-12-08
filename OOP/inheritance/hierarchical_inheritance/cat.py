e6af97265dbb14ab212341ef15e8cf9a013890bd
mialskywalker <mialskywalker@gmail.com>
2022-12-08 13:16:13 +0100

    08.12.2022

---
+++ OOP/inheritance/hierarchical_inheritance/cat.py
@@ -0,0 +1,7 @@
+from OOP.inheritance.hierarchical_inheritance.animal import Animal
+
+
+class Cat(Animal):
+
+    def meow(self):
+        return 'meowing...'
---
+++ OOP/inheritance/hierarchical_inheritance/dog.py
@@ -0,0 +1,7 @@
+from OOP.inheritance.hierarchical_inheritance.animal import Animal
+
+
+class Dog(Animal):
+
+    def bark(self):
+        return 'barking...'
